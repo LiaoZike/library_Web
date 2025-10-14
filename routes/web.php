7803c5167e78e1c1a99afdd25b2f6f27e76b6f97
LiaoZike <a0978435327@gmail.com>
2025-10-15 00:28:12 +0800

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/admin');
 
 Route::get('/API/{bookcaseID}', [APIController::class,'searchBookcase'])->name('API.searchBookcase');
